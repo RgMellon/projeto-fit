91a2fcf8b15ce256704545289e62097403aaef19
Renan <renan_gian@hotmail.com>
2018-06-17 20:33:03 -0300

    change url api

--- src/service/getAllPlanos.js
+++ src/service/getAllPlanos.js
@@ -1,4 +1,4 @@
 export const getAllPlanos = () => {
-  return fetch('http://localhost:8000/api/v1/planos')
+  return fetch('https://api-fit.herokuapp.com/api/v1/planos')
     .then(res => res.json())
 }
